f2fc64fb343c539d339598d4bf2810f69b7b7d9e
manmarru <soboru123@gmail.com>
2025-01-21 11:57:26 +0900

    30802 끝

---
+++ CodingTest/Q/30802.cpp
@@ -0,0 +1,32 @@
+#include "pch.h"
+#include "30802.h"
+#include <limits>
+
+void Solve(ifstream* pLoadStream)
+{
+	//T 씩 몇 묶음 사야 하는지
+	//P 자루씩 최대 몇 묶음 살 수 있는지, 그때 한자루씩 몇개 주문하는지
+
+	int iN(0);
+	int iSize[6] = { 0, 0, 0, 0, 0, 0 };
+	int iT(0), iP(0);
+	int iShirtAnswer(0);
+	(*pLoadStream) >> iN;
+
+	for (size_t i = 0; i < 6; i++)
+	{
+		(*pLoadStream) >> iSize[i];
+	}
+
+	(*pLoadStream) >> iT >> iP;
+	for (size_t i = 0; i < 6; i++)
+	{
+		iShirtAnswer += (iSize[i] / iT);
+		if (0 != iSize[i] % iT)
+			iShirtAnswer += 1;
+	}
+
+	cout << iShirtAnswer << '\n' << (iN / iP) << ' ' << iN % iP << endl;
+
+
+}
