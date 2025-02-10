5ffd4375312675efdd08f23e67dc98f752ae32b8
manmarru <soboru123@gmail.com>
2025-02-10 17:48:31 +0900

    10814 나이순정렬 끝 (Stable Sort &Inplace 공부)

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -1,6 +1,6 @@
 ﻿#pragma once
 #include "pch.h"
-#include "7568.h"
+#include "10814.h"
 
 int main()
 {
---
+++ CodingTest/Q/10814.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include "10814.h"
+#include <string>
+#include <list>
+#include <algorithm>
+
+void Solve(ifstream* pLoadStream)
+{
+	//Á¤·Ä
+	int iInput(0);
+	list<pair<int, string>> listPeople;
+	*pLoadStream >> iInput;
+
+	pair<int, string> People;
+	for (size_t i = 0; i < iInput; i++)
+	{
+		*pLoadStream >> People.first >> People.second;
+		listPeople.push_back(People);
+	}
+
+	listPeople.sort([](pair<int, string>Temp, pair<int, string>Src) ->bool
+		{
+			return Temp.first < Src.first;
+		});
+
+	for (auto pair : listPeople)
+	{
+		cout << pair.first << '\t' << pair.second << endl;
+	}
+}
