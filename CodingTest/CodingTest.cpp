e5410a75fe031fdfcdaea1dea488d739a4c294a6
manmarru <soboru123@gmail.com>
2025-02-18 16:22:54 +0900

    4949 균형잡힌 세상 끝

--- CodingTest/CodingTest.cpp
+++ CodingTest/CodingTest.cpp
@@ -5,7 +5,7 @@
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-
+	입출력최적화;
 
 
 
---
+++ CodingTest/Q/4949.cpp
@@ -0,0 +1,65 @@
+#include "pch.h"
+#include "Header.h"
+#include <stack>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	문자열에서 괄호 () [] {} 들이 다 닫혀있는지 확인하기
+	입력이 끝나면 .
+	*/
+
+	string szInput{};
+	bool endline{ true };
+	stack<char> stdBracket;
+	while (true)
+	{
+		getline(*pLoadStream, szInput);
+		for (char szTemp : szInput)
+		{
+
+			if (szTemp == '.')
+			{
+				if (endline)
+					return;
+				cout << (stdBracket.empty() ? "yes" : "no") << endl;
+				endline = true;
+				stack<char> stdTemp;
+				swap(stdBracket, stdTemp);
+				continue;
+			}
+
+			endline = false;
+
+			if (')' == szTemp)
+			{
+				if (stdBracket.empty())
+					stdBracket.push(szTemp);
+				else if (stdBracket.top() == '(')
+					stdBracket.pop();
+				else
+					stdBracket.push(szTemp);
+			}
+			else if ('}' == szTemp)
+			{
+				if (stdBracket.empty())
+					stdBracket.push(szTemp);
+				else if (stdBracket.top() == '{')
+					stdBracket.pop();
+				else
+					stdBracket.push(szTemp);
+			}
+			else if (']' == szTemp)
+			{
+				if (stdBracket.empty())
+					stdBracket.push(szTemp);
+				else if (stdBracket.top() == '[')
+					stdBracket.pop();
+				else
+					stdBracket.push(szTemp);
+			}
+			else if ('(' == szTemp || '{' == szTemp || '[' == szTemp)
+				stdBracket.push(szTemp);
+		}
+	}
+}
