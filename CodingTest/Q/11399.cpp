1a400b949e98fbddb88e1ad71a5368e97b27251d
manmarru <soboru123@gmail.com>
2025-02-24 21:46:19 +0900

    11399 ATM

---
+++ CodingTest/Q/11399.cpp
@@ -0,0 +1,31 @@
+#include "pch.h"
+#include "Header.h"
+#include <list>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	걍 정렬 아님? 시간제한이 1초긴 하네
+	*/
+
+	int iSize{};
+	long long int iInput{};
+	long long int iResult{ 0 };
+	list<long long int> listInput;
+
+	CIN >> iSize;
+	for (int i = 0; i < iSize; ++i)
+	{
+		CIN >> iInput;
+		listInput.push_back(iInput);
+	}
+	listInput.sort();
+
+	long long int iCurr{ 0 };
+	for (auto Temp : listInput)
+	{
+		iCurr += Temp;
+		iResult += iCurr;
+	}
+	cout << iResult;
+}
