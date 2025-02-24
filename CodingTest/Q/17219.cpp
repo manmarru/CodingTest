c7e3423338534a14c5c3a89eca3f4efb29824c5e
manmarru <soboru123@gmail.com>
2025-02-24 22:10:42 +0900

    17219 비밀번호 찾기

---
+++ CodingTest/Q/17219.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "Header.h"
+#include <string>
+#include <map>
+#include <unordered_map>
+void Solve(ifstream* pLoadStream)
+{
+	int iInput[2];
+	string szInput[2];
+	unordered_map<string, string> setPassWord;
+	szInput[0].reserve(20);
+	szInput[1].reserve(20);
+
+	CIN >> iInput[0] >> iInput[1];
+
+	for (int i = 0; i < iInput[0]; ++i)
+	{
+		CIN >> szInput[0] >> szInput[1];
+		setPassWord[szInput[0]] = szInput[1];
+	}
+
+	for (int i = 0; i < iInput[1]; ++i)
+	{
+		CIN >> szInput[0];
+		cout << setPassWord[szInput[0]] << '\n';
+	}
+
+}
