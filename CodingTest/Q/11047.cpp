5da4d121f6838970fd7476b35b1ecf874f9c00e5
manmarru <soboru123@gmail.com>
2025-02-24 21:01:22 +0900

    11047 동전 0

---
+++ CodingTest/Q/11047.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "Header.h"
+#include <stack>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	K원을 만들 수 있는 동전 갯수의 최솟값 구하기
+
+	N K
+	N개만큼 동전의 종류가 제시됨
+	*/
+	int iSize{};
+	int iTarget{};
+	CIN >> iSize >> iTarget;
+	stack<int> stlCoins;
+	int iInput{};
+	int iResult{ 0 };
+
+	for (int i = 0; i < iSize; i++)
+	{
+		CIN >> iInput;
+		stlCoins.push(iInput);
+	}
+
+	while (iTarget != 0)
+	{
+
+		iResult += (iTarget / stlCoins.top());
+		iTarget = iTarget % stlCoins.top();
+		stlCoins.pop();
+	}
+	cout << iResult;
+}
