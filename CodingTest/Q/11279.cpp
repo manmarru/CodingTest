e32f3f55e1c4651500076c9b2b98400f08e850fd
manmarru <soboru123@gmail.com>
2025-03-01 20:29:43 +0900

    11279 최대 힙

---
+++ CodingTest/Q/11279.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "Header.h"
+#include <queue>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	최대 힙
+	1. 삽입
+	2. 최대값 출력 + 값 삭제
+	*/
+	priority_queue<int> Temp;
+
+	int iSize;
+	CIN >> iSize;
+	int iInput;
+	for (int i = 0; i < iSize; i++)
+	{
+		CIN >> iInput;
+		if (0 == iInput)
+		{
+			if (Temp.empty())
+			{
+				cout << 0 << '\n';
+				continue;
+			}
+			cout << Temp.top() << '\n';
+			Temp.pop();
+		}
+		else
+		{
+			Temp.push(iInput);
+		}
+
+	}
+}
