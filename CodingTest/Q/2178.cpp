dfb8dea1bf8f079afd9a235a34b0b448c91490e7
manmarru <soboru123@gmail.com>
2025-03-05 13:07:38 +0900

    2178 미로 탐색

---
+++ CodingTest/Q/2178.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include "Header.h"
+#include <vector>
+#include <queue>
+#include <string>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	0,0 에서 반대쪽 끝까지 가는데 걸리는 횟수 구하기
+	https://www.acmicpc.net/source/90851936 한번 읽어보고 지우기
+	*/
+	
+	int iSizeY, iSizeX;
+	string szInput;
+	CIN >> iSizeY >> iSizeX;
+
+	vector<vector<int>> vecMap;
+	vecMap.resize(iSizeY);
+	for (int y = 0; y < iSizeY; y++)
+	{
+		vecMap[y].resize(iSizeX);
+		CIN >> szInput;
+		for (int x = 0; x < iSizeX; x++)
+		{
+			vecMap[y][x] = szInput[x] - '0';
+
+		}
+	}
+//여기까지 입력
+	queue<pair<int, int>> stlDestination;
+	stlDestination.push({ 0,0 });
+
+	int iX, iY;
+	while (1)
+	{
+		iY = stlDestination.front().first;
+		iX = stlDestination.front().second;
+		if ((iY == iSizeY - 1) && (iX == iSizeX - 1))
+			break;
+		stlDestination.pop();
+
+		if ((0 < iY) && (vecMap[iY - 1][iX] == 1))
+		{
+			stlDestination.push({ iY - 1, iX });
+			vecMap[iY - 1][iX] += vecMap[iY][iX];
+		}
+		if ((iSizeY - 1 > iY) && (vecMap[iY + 1][iX] == 1))
+		{
+			stlDestination.push({ iY + 1, iX });
+			vecMap[iY + 1][iX] += vecMap[iY][iX];
+		}
+		if ((0 < iX) && (vecMap[iY][iX - 1] == 1))
+		{
+			stlDestination.push({ iY, iX - 1 });
+			vecMap[iY][iX - 1] += vecMap[iY][iX];
+		}
+		if ((iSizeX - 1 > iX) && (vecMap[iY][iX + 1] == 1))
+		{
+			stlDestination.push({ iY, iX + 1 });
+			vecMap[iY][iX + 1] += vecMap[iY][iX];
+		}
+	}
+
+	cout << vecMap[iSizeY - 1][iSizeX - 1];
+}
