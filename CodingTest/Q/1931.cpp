59d8d88e0c1c8781b448e8c8cd5961243b02e05c
manmarru <soboru123@gmail.com>
2025-03-06 14:21:19 +0900

    1931 회의실 배정

---
+++ CodingTest/Q/1931.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "Header.h"
+#include <list>
+
+void Solve(ifstream* pLoadStream)
+{
+	/*
+	회의 시간표를 보고 가장 많이 넣을 수 있는 회의의 수를 출력
+	*/
+	int iSize;
+	CIN >> iSize;
+	int iInput[2];
+	list<pair<int, int>> listMeeting;
+
+	for (int i = 0; i < iSize; i++)
+	{
+		CIN >> iInput[0] >> iInput[1];
+		listMeeting.push_back({ iInput[0], iInput[1] });
+	}
+	listMeeting.sort();
+
+	/*
+	넣을 수 있는 회의중에 더 일찍 끝나는 회의가 있으면 그걸 선택
+	*/
+	pair<int, int> iCurrMeet{ listMeeting.front() };
+	int iResult{ 1 };
+	listMeeting.pop_front();
+	for (auto iter = listMeeting.begin(); iter != listMeeting.end(); iter++)
+	{
+		if (iCurrMeet.second > iter->second) // 겹침 + 더빨리끝나는회의
+		{
+			iCurrMeet = *iter;
+		}
+		else if (iCurrMeet.second <= iter->first) // 다음회의
+		{
+			++iResult;
+			iCurrMeet = *iter;
+		}
+	}
+	cout << iResult;
+}
--- CodingTest/pch.h
+++ CodingTest/pch.h
@@ -2,7 +2,7 @@
 #define PCH_H
 #define CIN *pLoadStream
 
-
+#define INTMAX 2147483648
 #include "Define.h"
 
 // 여기에 미리 컴파일하려는 헤더 추가
