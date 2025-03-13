8321debca4ff329835eb2bd24853fc03a2ca999c
manmarru <soboru123@gmail.com>
2025-03-13 11:13:25 +0900

    15652 N과M(4)

---
+++ CodingTest/Q/15652 N과M (4).cpp
@@ -0,0 +1,35 @@
+#include "pch.h"
+#include "Header.h"
+#include <vector>
+
+int MaxNum;
+int Pick;
+vector<int> Picked;
+void BFS(int _Curr, int _Index)
+{
+	Picked[_Index] = _Curr;
+	if (_Index == Pick)
+	{
+		for (int i = 1; i <= Pick; i++)
+		{
+			cout << Picked[i] << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	else
+	{
+		for (int i = _Curr; i <= MaxNum; i++)
+		{
+			BFS(i, _Index + 1);
+		}
+	}
+}
+
+void Solve(ifstream* pLoadStream)
+{
+	CIN >> MaxNum >> Pick;
+	Picked.resize(MaxNum + 1);
+	BFS(1, 0);
+	
+}
