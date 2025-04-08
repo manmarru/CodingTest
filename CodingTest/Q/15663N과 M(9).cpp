f14dd3a4bfaef206ff97b70aab4e73d4224d1e07
manmarru <soboru123@gmail.com>
2025-04-08 15:36:11 +0900

    15663 N과 M(9)

--- CodingTest/Q/15663N과 M(9).cpp
+++ CodingTest/Q/15663N과 M(9).cpp
@@ -2,19 +2,44 @@
 #include "Header.h"
 #include <vector>
 #include <algorithm>
+#include <set>
 
 int Size;
 int PickSize;
 vector<int> Nums;
 vector<bool> Picked;
-vector<int> PickValue;
+vector<int> Curr;
 
-void DFS(int _PickIndex	, int _Index)
+struct Compare
 {
-/*
-_Index 번째 선택으로 _Pickindex번째 숫자를 골랐다.
-*/
+	bool operator() (const vector<int>& Temp, const vector<int>& Src) const
+	{
+		return Temp < Src;
+	}
+};
+set<vector<int>, Compare> Result;
 
+void DFS(int Remain) // 몇개 더 골라야되는지
+{
+	if (Remain == 0)
+	{
+		Result.insert(Curr);
+		return;
+	}
+	else
+	{
+		for (int i = 0; i < Size; ++i)
+		{
+			if (!Picked[i])
+			{
+				Picked[i] = true;
+				Curr.push_back(Nums[i]);
+				DFS(Remain - 1);
+				Curr.pop_back();
+				Picked[i] = false;
+			}
+		}
+	}
 }
 
 void Solve(ifstream* _pLoadStream)
@@ -23,21 +48,26 @@ void Solve(ifstream* _pLoadStream)
 	N개의 자연수들 중
 	중복 없이 M개를 선택해서 만드는 수열을
 	오름차순으로 모두 구하기
+	https://www.acmicpc.net/source/92745880 모범답안
+	>> 같은 레벨에서 중복된 숫자를 안 건들기 위해 이전 값을 prew에 할당
 	*/
-
 	CIN >> Size >> PickSize;
-	Picked.resize(Size + 1, false);
-	Nums.resize(Size + 1);
-	PickValue.resize(PickSize + 1);
-	for (int i = 1; i <= Size; i++)
-	{
+	Nums.resize(Size);
+	Picked.resize(Size);
+	for (int i = 0; i < Size; ++i)
 		CIN >> Nums[i];
-	}
-	Nums.erase(unique(Nums.begin(), Nums.end()), Nums.end());
 	sort(Nums.begin(), Nums.end());
 
+	DFS(PickSize);
 
 
+	for (auto& vec : Result)
+	{
+		for (int Temp : vec)
+		{
+			cout << Temp << ' ';
+		}
+		cout << '\n';
+	}
 
-	DFS(0, 0);
 }
