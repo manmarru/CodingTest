86665464e647d94330fb320d0f18cd828849616f
manmarru <soboru123@gmail.com>
2025-06-27 12:50:26 +0900

    1005 ACM Craft

--- CodingTest/Q/1005 ACM Craft.cpp
+++ CodingTest/Q/1005 ACM Craft.cpp
@@ -1,15 +1,62 @@
 #include "pch.h"
 #include "Header.h"
+#include <vector>
+
+struct NODE
+{
+	int Cost;
+	vector<int> Link;
+	bool History = false;
+};
+
+vector<NODE> Nodes;
+
+int BackTrack(int NodeIndex)
+{
+	if (Nodes[NodeIndex].History) // 이미 연산 기록이 있다면
+	{
+		return Nodes[NodeIndex].Cost;
+	}
+
+	int MaxTime{ 0 };
+
+	for (int Prev : Nodes[NodeIndex].Link)
+	{
+		MaxTime = max(MaxTime, BackTrack(Prev));
+	}
+
+	Nodes[NodeIndex].Cost += MaxTime;
+	Nodes[NodeIndex].History = true;
+
+	return Nodes[NodeIndex].Cost;
+}
 
 void Solve(ifstream* _pLoadStream)
 {
-	int CaseSize;
+	int QuerySize;
 	int NodeSize;
 	int LinkSize;
-	CIN >> CaseSize >> NodeSize >> LinkSize;
+	CIN >> QuerySize;
 
-	for (int i = 0; i < LinkSize; ++i)
+	int Start, End;
+	for (int Query = 0; Query < QuerySize; ++Query)
 	{
+		CIN >> NodeSize >> LinkSize;
+		vector<NODE> Temp;
+		swap(Temp, Nodes);
+		Nodes.resize(NodeSize);
+		for (int i = 0; i < NodeSize; ++i)
+		{
+			CIN >> Nodes[i].Cost;
+		}
 
+		for (int i = 0; i < LinkSize; ++i)
+		{
+			CIN >> Start >> End;
+			Nodes[--End].Link.push_back(--Start);
+		}
+		CIN >> End;
+
+		cout << BackTrack(--End) << endl;
 	}
 }
